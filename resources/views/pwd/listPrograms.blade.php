0ac148fa945b0cc001ebdbeeb65e5a9f478d770c
evrylsem <evrylclaire.sembrino.21@usjr.edu.ph>
2024-07-21 16:25:31 +0800

    Update listPrograms.blade.php

--- resources/views/pwd/listPrograms.blade.php
+++ resources/views/pwd/listPrograms.blade.php
@@ -56,44 +56,44 @@
         </div>
         <div class="prog-grid">
             @if($filteredPrograms != null)
-            @foreach ($filteredPrograms as $filtered)
-            <div class="row prog-card mb-2">
-                <div class="col ">
-                    <a href="" class="d-flex prog-texts" data-id="{{ $filtered->id }}" onclick="openPopup(event)">
-                        <div class="prog-texts-container">
-                            <div class=" d-flex mb-2">
-                                <div class="prog-img"></div>
-                                <div class="d-flex justify-content-between">
-                                    <div class="header">
-                                        <h4>{{$filtered->title}}</h4>
-                                        <p class="sub-text">{{$filtered->agency->userInfo->name}}</p>
-                                        <p class="sub-text"><i class='bx bx-map sub-text'></i> {{$filtered->city}}</p>
+                @foreach ($filteredPrograms as $filtered)
+                <div class="row prog-card mb-2">
+                    <div class="col ">
+                        <a href="" class="d-flex prog-texts" data-id="{{ $filtered->id }}" onclick="openPopup(event)">
+                            <div class="prog-texts-container">
+                                <div class=" d-flex mb-2">
+                                    <div class="prog-img"></div>
+                                    <div class="d-flex justify-content-between">
+                                        <div class="header">
+                                            <h4>{{$filtered->title}}</h4>
+                                            <p class="sub-text">{{$filtered->agency->userInfo->name}}</p>
+                                            <p class="sub-text"><i class='bx bx-map sub-text'></i> {{$filtered->city}}</p>
+                                        </div>
+                                        <div class="text-end">
+                                            <p>{{ $filtered->created_at->format('M d, Y h:i A') }}</p>
+                                        </div>
                                     </div>
-                                    <div class="text-end">
-                                        <p>{{ $filtered->created_at->format('M d, Y h:i A') }}</p>
-                                    </div>
-                                </div>
 
-                            </div>
-                            <div class="row prog-desc mb-1">
-                                <p>{{$filtered->description}}</p>
-                            </div>
-                            <div class="row d-flex">
-                                <div class="match-info">
-                                    {{$filtered->disability->disability_name}}
                                 </div>
-                                <div class="match-info">
-                                    {{$filtered->education->education_name}}
+                                <div class="row prog-desc mb-1">
+                                    <p>{{$filtered->description}}</p>
+                                </div>
+                                <div class="row d-flex">
+                                    <div class="match-info">
+                                        {{$filtered->disability->disability_name}}
+                                    </div>
+                                    <div class="match-info">
+                                        {{$filtered->education->education_name}}
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        <div class="fs-3 d-flex flex-column align-items-center justify-content-center">
-                            >
-                        </div>
-                    </a>
+                            <div class="fs-3 d-flex flex-column align-items-center justify-content-center">
+                                >
+                            </div>
+                        </a>
+                    </div>
                 </div>
-            </div>
-            @endforeach
+                @endforeach
             @else
                 @foreach ($rankedPrograms as $ranked)
                 <div class="row prog-card mb-2">
