374c5317aa20a5177fc4617923023dfdf0196b2b
evrylsem <evrylclaire.sembrino.21@usjr.edu.ph>
2024-08-03 20:56:57 +0800

    pwd list progs edit

--- resources/views/pwd/listPrograms.blade.php
+++ resources/views/pwd/listPrograms.blade.php
@@ -53,61 +53,59 @@
                 </form>
             </div>
         </div>
-
-        <div class="prog-grid">
-            @foreach ($paginatedItems as $ranked)
-            <div class="row prog-card mb-2">
-                <div class="col ">
-                    <a href="{{ route('training-details', $ranked['program']->id ) }}" class="d-flex prog-texts">
-                        <div class="prog-texts-container">
-                            <div class=" d-flex mb-2">
-                                <div class="prog-img"></div>
-                                <div class="d-flex justify-content-between prog-head">
-                                    <div class="header">
-                                        <h4 class="text-cap">{{$ranked['program']->title}}</h4>
-                                        <p class="sub-text text-cap">{{$ranked['program']->agency->userInfo->name}}</p>
-                                        <p class="sub-text text-cap"><i class='bx bx-map sub-text'></i>{{(str_contains($ranked['program']->city, 'City') ? $ranked['program']->city : $ranked['program']->city . ' City')}}</p>
+        <div class="outer">
+            <div class="prog-grid" id="prog-grid">
+                @foreach ($paginatedItems as $ranked)
+                <div class="row prog-card mb-2">
+                    <div class="col ">
+                        <a href="{{ route('training-details', $ranked['program']->id ) }}" class="d-flex prog-texts">
+                            <div class="prog-texts-container">
+                                <div class=" d-flex mb-2">
+                                    <div class="prog-img"></div>
+                                    <div class="d-flex justify-content-between prog-head">
+                                        <div class="header">
+                                            <h4 class="text-cap">{{$ranked['program']->title}}</h4>
+                                            <p class="sub-text text-cap">{{$ranked['program']->agency->userInfo->name}}</p>
+                                            <p class="sub-text text-cap"><i class='bx bx-map sub-text'></i>{{(str_contains($ranked['program']->city, 'City') ? $ranked['program']->city : $ranked['program']->city . ' City')}}</p>
+                                        </div>
+                                        <div class="text-end date-posted">
+                                            <p class="text-end">{{ $ranked['program']->created_at->diffForHumans() }}</p>
+                                        </div>
                                     </div>
-                                    <div class="text-end date-posted">
-                                        <p class="text-end">{{ $ranked['program']->created_at->diffForHumans() }}</p>
+
+                                </div>
+                                <div class="row prog-desc mb-1">
+                                    <p>{{$ranked['program']->description}}</p>
+                                </div>
+                                <div class="row d-flex">
+                                    <div class="match-info">
+                                        {{$ranked['program']->disability->disability_name}}
+                                    </div>
+                                    <div class="match-info">
+                                        {{$ranked['program']->education->education_name}}
                                     </div>
                                 </div>
-
                             </div>
-                            <div class="row prog-desc mb-1">
-                                <p>{{$ranked['program']->description}}</p>
+                            <div class="fs-3 d-flex flex-column align-items-center justify-content-center">
+                                >
                             </div>
-                            <div class="row d-flex">
-                                <div class="match-info">
-                                    {{$ranked['program']->disability->disability_name}}
-                                </div>
-                                <div class="match-info">
-                                    {{$ranked['program']->education->education_name}}
-                                </div>
-                            </div>
-                        </div>
-                        <div class="fs-3 d-flex flex-column align-items-center justify-content-center">
-                            >
-                        </div>
-                    </a>
+                        </a>
+                    </div>
                 </div>
-            </div>
-            @endforeach
-            <div class="pagination">
-                {{ $paginatedItems->links() }}
-            </div>
+                @endforeach
+                <div class="pagination">
+                    {{ $paginatedItems->links() }}
+                </div>
 
+            </div>
         </div>
+
     </div>
 </div>
-
-@endsection
 
 <script>
     function submitForm() {
         document.getElementById('filterForm').submit();
-
-
     }
 
     function checkAndSubmit() {
@@ -117,5 +115,5 @@
         }
     }
 
-
 </script>
+@endsection
